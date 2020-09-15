0d207e4e8f534e88965ebd882f0c0a6bef9bcead
Julio Fernandez <photodad@outlook.com>
2020-09-14 23:23:56 -0700

    Taking out appid value

--- getWeather/getSDWeather.py
+++ getWeather/getSDWeather.py
@@ -3,7 +3,7 @@ import requests
 # Check current San Diego Weather
 response = requests.get(
     'http://api.openweathermap.org/data/2.5/weather',
-    params={'zip':'92129,us', 'units':'imperial', 'appid':'83facee3695919d0ad17c8b600e459f7'},
+    params={'zip':'92129,us', 'units':'imperial', 'appid':'*****************'},
 )
 
 # Inspect weather response
