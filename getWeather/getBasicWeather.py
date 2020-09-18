712966f31f99cd1c9e15cdaf53123c5695056cfb
Julio Fernandez <photodad@outlook.com>
2020-09-17 17:14:40 -0700

    Updating weather scripts

--- getWeather/getBasicWeather.py
+++ getWeather/getBasicWeather.py
@@ -1,4 +1,5 @@
-import requests, json
+import requests
+import json
 
 # Set the request parameters
 url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22San%20Diego%2C%20ca%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"'
@@ -21,4 +22,4 @@ data = response.json()
 
 #Print forecast for each of next 10 days
 for element in data['query']['results']['channel']['item']['forecast']:
-      print json.dumps(element)
+      print (json.dumps(element))
--- getWeather/getSDWeather.py
+++ getWeather/getSDWeather.py
@@ -1,9 +1,11 @@
+#!/usr/bin/python3.8
+
 import requests
 
 # Check current San Diego Weather
 response = requests.get(
     'http://api.openweathermap.org/data/2.5/weather',
-    params={'zip':'92129,us', 'units':'imperial', 'appid':'*****************'},
+    params={'zip':'92129,us', 'units':'imperial', 'appid':'83facee3695919d0ad17c8b600e459f7'},
 )
 
 # Inspect weather response
